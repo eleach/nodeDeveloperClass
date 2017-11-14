3c8ff389ffff36228912ebae1deccea8f4c3546d
eleach <eleach2@yahoo.com>
2017-11-14 17:22:21 -0600

    Setup Heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 // handlebars
@@ -83,7 +85,8 @@ app.get('/bad', (req, res) => {
 	});
 });
 
-app.listen(3000, () => {
-	console.log( 'server is up and running'   );
+
+app.listen(port, () => {
+	console.log( `Server is running on port ${port}` );
 });
 
