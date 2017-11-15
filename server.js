283972ecbfbb86a6913c1191be58eb161eb5beb3
eleach <eleach2@yahoo.com>
2017-11-14 18:21:31 -0600

    copy from andrew

--- server.js
+++ server.js
@@ -1,92 +1,56 @@
 const express = require('express');
 const hbs = require('hbs');
-fs = require('fs');
+const fs = require('fs');
 
 const port = process.env.PORT || 3000;
-
 var app = express();
 
-// handlebars
-
-// defaults to 'views' directory
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
 
-hbs.registerPartials(__dirname + '/views/partials');
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
-	return new Date().getFullYear()
+  return new Date().getFullYear();
 });
 
 hbs.registerHelper('screamIt', (text) => {
-	return text.toUpperCase();
-})
-
-// express
-app.use( express.static( __dirname + '/public'));
-
-app.use((req, res, next) => {
-	var now = new Date().toString();
-	var log =  `${now} :: ${req.method} :: ${req.url}` ;
-	console.log( log   );
-	fs.appendFile('server.log.txt', log + '\n', (err) => {
-		if (err) {
-			console.log( 'Unable to append to server log\n\n' + err   );
-			}
-	}); // log fs append
-	next();
-
-});
-
-app.use((req, res, next) => {
-
-	res.render('mainenance.hbs');
-
-	// next();
-})
-
-// app.get('/', (req, res) => {
-//
-// 	// res.send('<h1>hello eexpress</h1>');
-//
-// 	res.type('json');
-//
-// 	res.send ({
-// 		name: 'e',
-// 		likes: ['read', 'stuff']
-// 	})
-// });
-
-app.get('/about', (req, res) => {
-
-	// res.send('About page.');
-
-		res.render('about.hbs', {
-		pageTitle: 'Abouttt page Title',
-		currentYear: new Date().getFullYear(),
-		message: 'Lousy message test.'
-	});
+  return text.toUpperCase();
 });
 
 app.get('/', (req, res) => {
-
-	// res.send('About page.');
-
-		res.render('home.hbs', {
-		pageTitle: 'HOme page Title',
-		currentYear: new Date().getFullYear(),
-		welcome: 'Welcome mmessages'
-	});  // close render
-});  // close get
-
-app.get('/bad', (req, res) => {
-	res.send({
-		error: 'Unable to handle request.'
-	});
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
 });
 
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
 
 app.listen(port, () => {
-	console.log( `Server is running on port ${port}` );
+  console.log(`Server is up on port ${port}`);
 });
-
